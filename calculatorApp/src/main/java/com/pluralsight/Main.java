f418f4d66c6407b8e7282bb6714c300fc6276d35
Daja Miller <dajamiller96@gmail.com>
2025-09-25 09:43:53 -0700

    re-created project

---
+++ calculatorApp/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println("Sum = " + (a + b));
+        }
+    }
+}
