e2b479270feff6b19b8e5bc898355f2572ecf1b1
Daja Miller <dajamiller96@gmail.com>
2025-09-25 12:28:52 -0700

    Math application

---
+++ workbook1d/StringApplication/src/main/java/com/pluralsight/receipt.java
@@ -0,0 +1,8 @@
+public class Receipt {
+    public static void mani(String[] args {
+// step 3
+        String itemName = "shoes" ;
+        double itemPrice = 129.99
+        System.out.println("You bought 1 pair of " + itemName + " for $" + itemPrice);
+    })
+}
