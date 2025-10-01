cf9eb271429b53d62eacd543d645fa26c5863908
Daja Miller <dajamiller96@gmail.com>
2025-10-01 11:51:35 -0700

    variable and loop exercises completed

---
+++ AI-Creative-Teaching/VariablesStringsMethods/src/main/java/com/pluralsight/Variables.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public class Variables {
+    public static void main(String args[]) {
+
+        // variables - strings, ints, booleans
+        // 1. Declare the type and the name, then assign a value!
+        int kittyHealth = 100;
+
+        // 2. A variable to store a string of text
+        String currentZone = "Sparkle Meadow";
+
+        // 3. A boolean to check if Winnie has her tiara on
+        boolean hasTiara = true;
+
+        // Exercise: Write a single line of Java code to create an integer
+        // variable called adventureScore and set its initial value to 500.
+
+        int adventureScore = 500;
+
+    }
+}
