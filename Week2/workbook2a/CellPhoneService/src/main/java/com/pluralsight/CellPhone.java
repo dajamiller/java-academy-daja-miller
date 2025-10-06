aafd734aa37339c5a3703a22ad251349d7d12b10
Daja Miller <dajamiller96@gmail.com>
2025-10-06 14:35:59 -0700

    exercise 6 complete

--- Week2/workbook2a/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
+++ Week2/workbook2a/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
@@ -4,12 +4,57 @@ public class CellPhone {
     //instance variables
     //all private - type - name
     private String model;
-    private long serialNumber;
+    private String serialNumber;
     private String carrier;
     private String phoneNumber;
     private String ownerName;
+    // ----------------------------default constructor
 
-    public CellPhone(String model, long serialNumber, String carrier, String phoneNumber, String ownerName) {
+
+    //---------------------------- parameterized constructor
+
+    public CellPhone(String model,  String serialNumber, String carrier, String phoneNumber, String ownerName) {
+        this.model = model;
+        this.serialNumber = serialNumber;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.ownerName = ownerName;
+    }
+    //  -------------------------------------------- getters
+    public String getModel() {
+        return model;
+    }
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+    public String getCarrier() {
+        return carrier;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+
+    public CellPhone() {
+    }
+    // ------------------------------------------------- setters
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
     }
 
 }
--- Week2/workbook2a/CellPhoneService/src/main/java/com/pluralsight/Main.java
+++ Week2/workbook2a/CellPhoneService/src/main/java/com/pluralsight/Main.java
@@ -1,15 +1,48 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        // create object
+        CellPhone cellPhone = new CellPhone();
+        // use scanner to ask for details
+        System.out.println("Welcome to CellPhone Info Retrieval Tool!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("To get started, Please enter your phones serial number (10-12 digits): ");
+        String serialNumber = scanner.nextLine().trim();
+        System.out.println("Next, enter your phone model (ex. iPhone X): ");
+        String model = scanner.nextLine().trim();
+        System.out.println("Who is your phone carrier? (ex. T-Mobile):");
+        String carrier = scanner.nextLine().trim();
+        System.out.println("Please enter your phone number (ex. 281-330-8004)");
+        String phoneNumber = scanner.nextLine().trim();
+        System.out.println("Please enter the cell phone owner's name (ex. Mike Jones): ");
+        String ownerName = scanner.nextLine().trim();
+
+        // store values in object - using setters
+        cellPhone.setOwnerName(ownerName);
+        cellPhone.setPhoneNumber(phoneNumber);
+        cellPhone.setCarrier(carrier);
+        cellPhone.setModel(model);
+        cellPhone.setSerialNumber(serialNumber);
+
+        // display stored info
+        System.out.println("\n\n**Here is your Cell Phone Information: \n");
+        //System.out.println("Owner: " + ownerName + " \n" + "Carrier: " + carrier + " \n" + "Phone number: " + phoneNumber + " \n"  +  "Model: " + model + " \n" + "Serial Number: " + serialNumber);
+
+        // return with getters
+        System.out.println("Owner: " + cellPhone.getOwnerName());
+        System.out.println("Phone: " + cellPhone.getPhoneNumber());
+        System.out.println("Carrier: " + cellPhone.getCarrier());
+        System.out.println("Model: " + cellPhone.getModel());
+        System.out.println("Serial Number: " + cellPhone.getSerialNumber());
+
     }
-
-
-
-    }
+}
 
 
 
