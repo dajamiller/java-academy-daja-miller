75061341fae617267cdc50586af6a5eacae8a712
Daja Miller <dajamiller96@gmail.com>
2025-10-06 11:06:47 -0700

    progress

---
+++ Week2/workbook2a/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class CellPhone {
+    //instance variables
+    //all private - type - name
+    private String model;
+    private long serialNumber;
+    private String carrier;
+    private String phoneNumber;
+    private String ownerName;
+
+    public CellPhone(String model, long serialNumber, String carrier, String phoneNumber, String ownerName) {
+    }
+
+}
