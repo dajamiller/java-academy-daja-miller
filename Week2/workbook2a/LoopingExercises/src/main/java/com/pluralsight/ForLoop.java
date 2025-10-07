fa1df9453630443fb4a64b881c8d9472eefda70a
Daja Miller <dajamiller96@gmail.com>
2025-10-07 10:44:24 -0700

    exercise complete

--- Week2/workbook2a/LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
+++ Week2/workbook2a/LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
@@ -2,10 +2,11 @@ package com.pluralsight;
 
 public class ForLoop {
     public static void main(String[] args) throws InterruptedException {
-        int j = 0;
-        for (int j = 10; j >= 1; j--); {
-            System.out.println(j);
+        System.out.println("Countdown to launch:");
+        for (int f = 10; f >= 1; f--) {
+            System.out.println(f);
             Thread.sleep(1000);
         }
+        System.out.println("Launch!");
     }
 }
